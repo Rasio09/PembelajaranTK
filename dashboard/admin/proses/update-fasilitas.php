65ffec657ed1fcfe8022e040ff8d834b1337e486
Rasio09 <rasiovolume22@gmail.com>
2025-03-05 12:43:02 +0700

    update dashboard data fasilitas bug ubah data

--- dashboard/admin/proses/update-fasilitas.php
+++ dashboard/admin/proses/update-fasilitas.php
@@ -3,14 +3,14 @@ include ("../../connect.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = $_POST['id'];
-    $judul = $_POST['judul'];
-    $isi = $_POST['isi'];
-    $icon = $_POST['icon'];
-    $color = $_POST['color'];
+    $judul = $_POST['judul_fasilitas'];
+    $isi = $_POST['isi_fasilitas'];
+    $icon = $_POST['icon_fasilitas'];
+    $color = $_POST['color_hover'];
 
     // Update data ke database
-    $query = $conn->prepare("UPDATE tb_fasilitas SET judul_fasilitas=?, isi_fasilitas=?, icon=?, color_hover=? WHERE id_fasilitas=?");
-    $query->bind_param("sssssi", $judul, $isi, $icon, $color, $id);
+    $query = $conn->prepare("UPDATE tb_fasilitas SET judul_fasilitas='$judul', isi_fasilitas='$isi', icon='$icon', color_hover='$color' WHERE id_fasilitas=$id");
+
 
     if ($query->execute()) {
         echo "<script>alert('Data berhasil diperbarui!'); window.location.href='../data-fasilitas.php';</script>";
